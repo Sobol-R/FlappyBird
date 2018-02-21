a1a3a5524a1218357e1adc079b19d435f72d405d
Sobol-R <rostislav.sobolev@gmail.com>
2018-02-22 00:33:51 +0300

    preLast version with some problems

--- app/src/main/java/com/example/user/flappybird/MainActivity.java
+++ app/src/main/java/com/example/user/flappybird/MainActivity.java
@@ -15,15 +15,22 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     ImageView banana;
-    ImageView monkey;
+    View monkey;
+    View monkey2;
     RelativeLayout root;
     RelativeLayout menu;
     TextView start;
+    TextView score;
 
     float bananaV;
     int bananaY;
     int monkeyX;
+    int monkey2X;
+    int monkey2Y;
     int monkeyY;
+    boolean check = false;
+    int n = 3;
+    int sc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         start = findViewById(R.id.start);
         banana = findViewById(R.id.banana);
         monkey = findViewById(R.id.obstacle);
+        monkey2 = findViewById(R.id.obstacle2);
         root = findViewById(R.id.root);
+        score = findViewById(R.id.score);
         startNewGame();
         root.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,45 +58,70 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 menu.setVisibility(View.GONE);
+                monkey.setVisibility(View.VISIBLE);
                 bananaV = 0;
                 bananaY = 0;
                 monkeyX = 500;
                 monkeyY = 0;
+                check = true;
                 initializeTimer();
             }
         });
     }
     private void initializeTimer() {
-        final Handler handler = new Handler();
-        new Timer().scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                onTimer();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        onTimerUi();
-                    }
-                });
-            }
-        }, 25, 25);
+            final Handler handler = new Handler();
+            new Timer().scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    onTimer();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            onTimerUi();
+                        }
+                    });
+                }
+            }, 25, 25);
     }
 
     private void onTimer() {
-        monkeyX -= 3;
-        if (monkey.getTranslationX() < -500) {
+        if (check == true) {
+            if (n > 10) {
+                monkey2X -= n;
+            }
+            monkeyX -= n;
+            if (monkey.getTranslationX() < -500) {
+                sc += 1;
+                monkeyX = 500;
+                monkeyY = (int) (Math.random() * 700 - 200);
+                if (n < 20) {
+                    n += 2;
+                }
+            }
+            if (monkey2.getTranslationX() < -500) {
+                monkey2X = 500;
+                monkey2Y = (int) (Math.random() * 1000 - 200);
+            }
+            bananaV += 2;
+            bananaY += bananaV;
+        } else {
+            bananaV = 0;
+            bananaY = 0;
             monkeyX = 500;
-            monkeyY = (int) (Math.random() * 400 - 200);
+            monkeyY = 0;
         }
-        bananaV += 2;
-        bananaY += bananaV;
+
     }
 
     private void onTimerUi() {
         banana.setTranslationY(bananaY);
         monkey.setTranslationX(monkeyX);
         monkey.setTranslationY(monkeyY);
-        if (bananaY > 1280 || bananaY < -1280) {
+        if (bananaY > 1280 || bananaY < -1280 || bananaY == monkeyY || bananaY == monkey2Y) {
+            monkey.setVisibility(View.GONE);
+            score.setVisibility(View.VISIBLE);
+            score.setText("score :  " + sc);
+            check = false;
             startNewGame();
         }
     }
